66b2d6cf26afbdeaa7bbb63de8cf7f65157dea75
JordyCA <jordycastroavendano@gmail.cm>
2024-02-16 16:12:00 -0600

    update files

---
+++ public/importExcel/noticiaAdministrativa/pbr/sql/Update2024Normales-Validado.sql
@@ -0,0 +1,44 @@
+INSERT INTO `595071_accionespue`.`noticia_administrativa_conceptos_pbr` (`id_actividades`, `id_noticia_administrativa`, `id_dependencia`, `id_unidad_responsable`, `id_eje`, `id_programa`, `lineas_accion`, `orden_concepto`, `is_indicador_mensual`, `eliminado`) 
+VALUES ('5071', '1202', '2', '9', '2', '2', '[2,7,9]', '20', '1', '0'); 
+INSERT INTO `595071_accionespue`.`noticia_administrativa_conceptos_pbr` (`id_actividades`, `id_noticia_administrativa`, `id_dependencia`, `id_unidad_responsable`, `id_eje`, `id_programa`, `lineas_accion`, `orden_concepto`, `is_indicador_mensual`, `eliminado`) 
+VALUES ('5092', '1216', '2', '214', '2', '2', '[10,15]', '46', '1', '0'); 
+INSERT INTO `595071_accionespue`.`noticia_administrativa_conceptos_pbr` (`id_actividades`, `id_noticia_administrativa`, `id_dependencia`, `id_unidad_responsable`, `id_eje`, `id_programa`, `lineas_accion`, `orden_concepto`, `is_indicador_mensual`, `eliminado`) 
+VALUES ('5184', '1628', '4', '237', '2', '4', '[6,7]', '8', '1', '0'); 
+INSERT INTO `595071_accionespue`.`noticia_administrativa_conceptos_pbr` (`id_actividades`, `id_noticia_administrativa`, `id_dependencia`, `id_unidad_responsable`, `id_eje`, `id_programa`, `lineas_accion`, `orden_concepto`, `is_indicador_mensual`, `eliminado`) 
+VALUES ('5187', '1633', '4', '238', '2', '4', '[16,19,20]', '15', '1', '0'); 
+INSERT INTO `595071_accionespue`.`noticia_administrativa_conceptos_pbr` (`id_actividades`, `id_noticia_administrativa`, `id_dependencia`, `id_unidad_responsable`, `id_eje`, `id_programa`, `lineas_accion`, `orden_concepto`, `is_indicador_mensual`, `eliminado`) 
+VALUES ('5188', '1431', '4', '238', '2', '4', '[20]', '16', '1', '0'); 
+INSERT INTO `595071_accionespue`.`noticia_administrativa_conceptos_pbr` (`id_actividades`, `id_noticia_administrativa`, `id_dependencia`, `id_unidad_responsable`, `id_eje`, `id_programa`, `lineas_accion`, `orden_concepto`, `is_indicador_mensual`, `eliminado`) 
+VALUES ('5193', '1434', '4', '238', '2', '4', '[]', '21', '1', '0'); 
+INSERT INTO `595071_accionespue`.`noticia_administrativa_conceptos_pbr` (`id_actividades`, `id_noticia_administrativa`, `id_dependencia`, `id_unidad_responsable`, `id_eje`, `id_programa`, `lineas_accion`, `orden_concepto`, `is_indicador_mensual`, `eliminado`) 
+VALUES ('5237', '961', '6', '25', '2', '6', '[44,47]', '12', '1', '0'); 
+INSERT INTO `595071_accionespue`.`noticia_administrativa_conceptos_pbr` (`id_actividades`, `id_noticia_administrativa`, `id_dependencia`, `id_unidad_responsable`, `id_eje`, `id_programa`, `lineas_accion`, `orden_concepto`, `is_indicador_mensual`, `eliminado`) 
+VALUES ('5238', '962', '6', '25', '2', '6', '[43,50]', '13', '1', '0'); 
+INSERT INTO `595071_accionespue`.`noticia_administrativa_conceptos_pbr` (`id_actividades`, `id_noticia_administrativa`, `id_dependencia`, `id_unidad_responsable`, `id_eje`, `id_programa`, `lineas_accion`, `orden_concepto`, `is_indicador_mensual`, `eliminado`) 
+VALUES ('5243', '964', '6', '26', '2', '6', '[35,36,37,39,40]', '16', '1', '0'); 
+INSERT INTO `595071_accionespue`.`noticia_administrativa_conceptos_pbr` (`id_actividades`, `id_noticia_administrativa`, `id_dependencia`, `id_unidad_responsable`, `id_eje`, `id_programa`, `lineas_accion`, `orden_concepto`, `is_indicador_mensual`, `eliminado`) 
+VALUES ('5244', '965', '6', '26', '2', '6', '[7,8,9,14]', '17', '1', '0'); 
+INSERT INTO `595071_accionespue`.`noticia_administrativa_conceptos_pbr` (`id_actividades`, `id_noticia_administrativa`, `id_dependencia`, `id_unidad_responsable`, `id_eje`, `id_programa`, `lineas_accion`, `orden_concepto`, `is_indicador_mensual`, `eliminado`) 
+VALUES ('5247', '968', '6', '26', '2', '6', '[3,5,6,18,22,41,44,49]', '20', '1', '0'); 
+INSERT INTO `595071_accionespue`.`noticia_administrativa_conceptos_pbr` (`id_actividades`, `id_noticia_administrativa`, `id_dependencia`, `id_unidad_responsable`, `id_eje`, `id_programa`, `lineas_accion`, `orden_concepto`, `is_indicador_mensual`, `eliminado`) 
+VALUES ('5259', '978', '6', '26', '2', '6', '[]', '30', '1', '0'); 
+INSERT INTO `595071_accionespue`.`noticia_administrativa_conceptos_pbr` (`id_actividades`, `id_noticia_administrativa`, `id_dependencia`, `id_unidad_responsable`, `id_eje`, `id_programa`, `lineas_accion`, `orden_concepto`, `is_indicador_mensual`, `eliminado`) 
+VALUES ('5276', '873', '7', '174', '2', '7', '[1,4,5,9,10,13,16,17,18]', '13', '1', '0'); 
+INSERT INTO `595071_accionespue`.`noticia_administrativa_conceptos_pbr` (`id_actividades`, `id_noticia_administrativa`, `id_dependencia`, `id_unidad_responsable`, `id_eje`, `id_programa`, `lineas_accion`, `orden_concepto`, `is_indicador_mensual`, `eliminado`) 
+VALUES ('5277', '874', '7', '174', '2', '7', '[]', '14', '1', '0'); 
+INSERT INTO `595071_accionespue`.`noticia_administrativa_conceptos_pbr` (`id_actividades`, `id_noticia_administrativa`, `id_dependencia`, `id_unidad_responsable`, `id_eje`, `id_programa`, `lineas_accion`, `orden_concepto`, `is_indicador_mensual`, `eliminado`) 
+VALUES ('5287', '888', '7', '178', '2', '7', '[]', '28', '1', '0'); 
+INSERT INTO `595071_accionespue`.`noticia_administrativa_conceptos_pbr` (`id_actividades`, `id_noticia_administrativa`, `id_dependencia`, `id_unidad_responsable`, `id_eje`, `id_programa`, `lineas_accion`, `orden_concepto`, `is_indicador_mensual`, `eliminado`) 
+VALUES ('5291', '893', '7', '179', '2', '7', '[1,4,7,9,13]', '33', '1', '0'); 
+INSERT INTO `595071_accionespue`.`noticia_administrativa_conceptos_pbr` (`id_actividades`, `id_noticia_administrativa`, `id_dependencia`, `id_unidad_responsable`, `id_eje`, `id_programa`, `lineas_accion`, `orden_concepto`, `is_indicador_mensual`, `eliminado`) 
+VALUES ('5292', '894', '7', '179', '2', '7', '[]', '34', '1', '0'); 
+INSERT INTO `595071_accionespue`.`noticia_administrativa_conceptos_pbr` (`id_actividades`, `id_noticia_administrativa`, `id_dependencia`, `id_unidad_responsable`, `id_eje`, `id_programa`, `lineas_accion`, `orden_concepto`, `is_indicador_mensual`, `eliminado`) 
+VALUES ('5299', '864', '7', '171', '2', '7', '[19,20]', '4', '1', '0'); 
+INSERT INTO `595071_accionespue`.`noticia_administrativa_conceptos_pbr` (`id_actividades`, `id_noticia_administrativa`, `id_dependencia`, `id_unidad_responsable`, `id_eje`, `id_programa`, `lineas_accion`, `orden_concepto`, `is_indicador_mensual`, `eliminado`) 
+VALUES ('5680', '1545', '13', '273', '4', '13', '[30,43]', '15', '1', '0'); 
+INSERT INTO `595071_accionespue`.`noticia_administrativa_conceptos_pbr` (`id_actividades`, `id_noticia_administrativa`, `id_dependencia`, `id_unidad_responsable`, `id_eje`, `id_programa`, `lineas_accion`, `orden_concepto`, `is_indicador_mensual`, `eliminado`) 
+VALUES ('5698', '1550', '13', '275', '4', '13', '[5,15,16,19,20,21]', '20', '1', '0'); 
+INSERT INTO `595071_accionespue`.`noticia_administrativa_conceptos_pbr` (`id_actividades`, `id_noticia_administrativa`, `id_dependencia`, `id_unidad_responsable`, `id_eje`, `id_programa`, `lineas_accion`, `orden_concepto`, `is_indicador_mensual`, `eliminado`) 
+VALUES ('6244', '1476', '26', '253', '5', '26', '[3,4]', '33', '1', '0'); 
+INSERT INTO `595071_accionespue`.`noticia_administrativa_conceptos_pbr` (`id_actividades`, `id_noticia_administrativa`, `id_dependencia`, `id_unidad_responsable`, `id_eje`, `id_programa`, `lineas_accion`, `orden_concepto`, `is_indicador_mensual`, `eliminado`) 
+VALUES ('6244', '1472', '26', '136', '5', '26', '[3,4]', '28', '1', '0'); 
--- public/importExcel/usuarios/onlyPasswords/password.sql
+++ public/importExcel/usuarios/onlyPasswords/password.sql
@@ -1,2 +1,2 @@
-### Password - Pue@oHsoI9v / $2b$12$k.I7Vm6sme0UPwFcTbQc/.G5g5rFswvkLXy8psBM9TA8hT7hpiDAa 
+### Password - Pue@IeKZQfre / $2b$12$d3hTdSfm0J9rReylecui2.UwFdCtI4I9QtX5gusB4KveSa3ZA19Bm 
   
